3547c0e13a24851290e3ac023a1375770e6befac
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2025-10-06 19:31:27 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -22,3 +22,34 @@ const switchLang = () => {
 document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
 document.querySelector('#desc-switcher').addEventListener('click', switchDesc)
 document.querySelector('#lang-switcher').addEventListener('click', switchLang)
+
+document.querySelectorAll('.item-text-code').forEach(span => {
+    span.addEventListener('click', copyCommand);
+});
+
+async function copyCommand(event) {
+    const element = event.currentTarget;
+    const text = element.textContent;
+
+    try {
+        await navigator.clipboard.writeText(text);
+    } catch (err) {
+        const textArea = document.createElement('textarea');
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+    }
+    showFeedback(element);
+}
+
+function showFeedback(element) {
+    element.style.setProperty('border-color', 'var(--accent-text-dark)');
+    element.style.setProperty('background', 'var(--accent-text-light)');
+
+    setTimeout(() => {
+        element.style.setProperty('border-color', 'var(--accent-text-light)');
+        element.style.setProperty('background', 'var(--accent-text-dark)');
+    }, 100);
+}
